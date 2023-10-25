0067b2808f14abdc7a88165338f253593cb09154
Isaac Marcos <isaacmarcos@scdesign.com.br>
2023-10-25 10:17:40 -0300

    :sparkles: feat: add link in bio to workshop

--- src/pages/bio/index.tsx
+++ src/pages/bio/index.tsx
@@ -1,42 +1,134 @@
 import { Box, Flex, Image, Heading, Text, Icon, Link } from "@chakra-ui/react";
-import { BsTruckFlatbed } from 'react-icons/bs'
-import { HiMapPin } from 'react-icons/hi2'
+import { BsTruckFlatbed } from "react-icons/bs";
+import { HiMapPin } from "react-icons/hi2";
+import { SlUserFemale } from "react-icons/sl";
 import { LinkIcons } from "../../components/linkIcons";
 
 export default function Bio() {
-  
   return (
-    <Flex align="center" justify="center" bgImg="Fachada.jpg" bgSize="cover" bgColor="#000000f2" bgBlendMode="overlay" >
-      <Flex flexDir="column" w="100%" maxW="540px" bgColor="secundary" minH="100vh" align="center" px="40px" >
-
-        <Flex flexDir="column" pt="50px" align="center" >
-          <Box display="flex" boxSize="110px" borderRadius="full" bgColor="terciary" alignItems="center" justifyContent="center" >
+    <Flex
+      align="center"
+      justify="center"
+      bgImg="Fachada.jpg"
+      bgSize="cover"
+      bgColor="#000000f2"
+      bgBlendMode="overlay"
+    >
+      <Flex
+        flexDir="column"
+        w="100%"
+        maxW="540px"
+        bgColor="secundary"
+        minH="100vh"
+        align="center"
+        px="40px"
+      >
+        <Flex flexDir="column" pt="50px" align="center">
+          <Box
+            display="flex"
+            boxSize="110px"
+            borderRadius="full"
+            bgColor="terciary"
+            alignItems="center"
+            justifyContent="center"
+          >
             <Image src="/Aparbs Light 3.svg" boxSize="90px" align="center" />
           </Box>
-          <Heading mt="20px" color="terciary" textAlign="center" letterSpacing=".1rem" fontFamily="Montserrat, sans-serif" fontSize="25px" fontWeight="600" >
-            APARBS <br/> Soluções Automotivas
+          <Heading
+            mt="20px"
+            color="terciary"
+            textAlign="center"
+            letterSpacing=".1rem"
+            fontFamily="Montserrat, sans-serif"
+            fontSize="25px"
+            fontWeight="600"
+          >
+            APARBS <br /> Soluções Automotivas
           </Heading>
-          <Text w="100%" fontSize="14px" textTransform="uppercase" textAlign="center" letterSpacing=".1187rem" lineHeight="21px" fontStyle="oblique" mt="10px" >
-            O CENTRO AUTOMOTIVO QUE <br/> SEU CARRO SE SENTE SEGURO
+          <Text
+            w="100%"
+            fontSize="14px"
+            textTransform="uppercase"
+            textAlign="center"
+            letterSpacing=".1187rem"
+            lineHeight="21px"
+            fontStyle="oblique"
+            mt="10px"
+          >
+            O CENTRO AUTOMOTIVO QUE <br /> SEU CARRO SE SENTE SEGURO
           </Text>
         </Flex>
 
-        <Flex w="100%" align="center" justify="center" mt="10" >
-          <LinkIcons/>
+        <Flex w="100%" align="center" justify="center" mt="10">
+          <LinkIcons />
         </Flex>
 
-        <Flex flexDir="column" w="100%" maxW="500px" gap="3" mt="3" >
-          
+        <Flex flexDir="column" w="100%" maxW="500px" gap="3" mt="3">
+          <Link
+            style={{ textDecoration: "none" }}
+            href="https://www.sympla.com.br/evento/oficina-rosa-conhecendo-seu-veiculo/2220495"
+            isExternal
+          >
+            <Box
+              display="flex"
+              w="100%"
+              maxH="60px"
+              minH="60px"
+              bgColor="pink.500"
+              p="6px"
+              alignItems="center"
+              gap="15px"
+              borderRadius="5"
+              _hover={{ bgColor: "terciary", color: "pink.500" }}
+              transition="0.3s"
+            >
+              <Box
+                display="flex"
+                minW="50px"
+                minH="50px"
+                borderRadius="5"
+                bgColor="terciary"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Icon as={SlUserFemale} boxSize="2rem" color="pink.500" />
+              </Box>
+              <Text fontSize="16px" fontWeight="500">
+                WorkShop Oficina Rosa
+              </Text>
+            </Box>
+          </Link>
+
           <Link
             style={{ textDecoration: "none" }}
             href="https://www.google.com/maps/dir//aparbs+porteirinha/@-15.755457,-43.0466363,14.17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x752281b048ee24b:0x8ce9b567298b1724!2m2!1d-43.0311318!2d-15.7536274?hl=pt-br"
             isExternal
           >
-            <Box display="flex" w="100%" maxH="60px" minH="60px" bgColor="primary" p="6px" alignItems="center" gap="15px" borderRadius="5" _hover={{ bgColor:"terciary", color:"primary" }} transition="0.3s" >
-              <Box display="flex" minW="50px" minH="50px" borderRadius="5" bgColor="terciary" alignItems="center" justifyContent="center" >
+            <Box
+              display="flex"
+              w="100%"
+              maxH="60px"
+              minH="60px"
+              bgColor="primary"
+              p="6px"
+              alignItems="center"
+              gap="15px"
+              borderRadius="5"
+              _hover={{ bgColor: "terciary", color: "primary" }}
+              transition="0.3s"
+            >
+              <Box
+                display="flex"
+                minW="50px"
+                minH="50px"
+                borderRadius="5"
+                bgColor="terciary"
+                alignItems="center"
+                justifyContent="center"
+              >
                 <Icon as={HiMapPin} boxSize="2rem" color="primary" />
               </Box>
-              <Text fontSize="16px" fontWeight="500" >
+              <Text fontSize="16px" fontWeight="500">
                 APARBS Porteirinha/MG
               </Text>
             </Box>
@@ -44,17 +136,37 @@ export default function Bio() {
 
           <Link
             style={{ textDecoration: "none" }}
-            href="https://www.google.com/maps/dir//APARBS+Solu%C3%A7%C3%B5es+Automotivas+-+Avenida+Salviana+Alves+da+Cruz,+70+-+Lourdes,+Riacho+dos+Machados+-+MG,+39529-000/@-16.0021619,-43.1143853,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x75237fd6501ef8f:0x6612bbf2e18cc73a!2m2!1d-43.044517!2d-16.0021732?hl=pt-br" isExternal
+            href="https://www.google.com/maps/dir//APARBS+Solu%C3%A7%C3%B5es+Automotivas+-+Avenida+Salviana+Alves+da+Cruz,+70+-+Lourdes,+Riacho+dos+Machados+-+MG,+39529-000/@-16.0021619,-43.1143853,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x75237fd6501ef8f:0x6612bbf2e18cc73a!2m2!1d-43.044517!2d-16.0021732?hl=pt-br"
+            isExternal
           >
-            <Box display="flex" w="100%" maxH="60px" minH="60px" bgColor="primary" p="6px" alignItems="center" gap="15px" borderRadius="5" _hover={{ bgColor:"terciary", color:"primary" }} transition="0.3s" >
-              <Box display="flex" minW="50px" minH="50px" borderRadius="5" bgColor="terciary" alignItems="center" justifyContent="center"  >
+            <Box
+              display="flex"
+              w="100%"
+              maxH="60px"
+              minH="60px"
+              bgColor="primary"
+              p="6px"
+              alignItems="center"
+              gap="15px"
+              borderRadius="5"
+              _hover={{ bgColor: "terciary", color: "primary" }}
+              transition="0.3s"
+            >
+              <Box
+                display="flex"
+                minW="50px"
+                minH="50px"
+                borderRadius="5"
+                bgColor="terciary"
+                alignItems="center"
+                justifyContent="center"
+              >
                 <Icon as={HiMapPin} boxSize="2rem" color="primary" />
               </Box>
-              <Text  fontSize="16px" fontWeight="500" >
+              <Text fontSize="16px" fontWeight="500">
                 APARBS Riacho dos Machados/MG
               </Text>
             </Box>
-
           </Link>
 
           <Link
@@ -62,24 +174,46 @@ export default function Bio() {
             href="https://api.whatsapp.com/send?phone=5538999944889&text=Olá%2C estou no site da APARBS. Gostaria de receber atendimento"
             isExternal
           >
-            <Box display="flex" w="100%" maxH="60px" minH="60px" bgColor="primary" p="6px" alignItems="center" gap="15px" borderRadius="5" _hover={{ bgColor:"terciary", color:"primary" }} transition="0.3s" >
-              <Box display="flex" minW="50px" minH="50px" borderRadius="5" bgColor="terciary" alignItems="center" justifyContent="center"  >
+            <Box
+              display="flex"
+              w="100%"
+              maxH="60px"
+              minH="60px"
+              bgColor="primary"
+              p="6px"
+              alignItems="center"
+              gap="15px"
+              borderRadius="5"
+              _hover={{ bgColor: "terciary", color: "primary" }}
+              transition="0.3s"
+            >
+              <Box
+                display="flex"
+                minW="50px"
+                minH="50px"
+                borderRadius="5"
+                bgColor="terciary"
+                alignItems="center"
+                justifyContent="center"
+              >
                 <Icon as={BsTruckFlatbed} boxSize="2rem" color="primary" />
               </Box>
-              <Text  fontSize="16px" fontWeight="500" >
+              <Text fontSize="16px" fontWeight="500">
                 APARBS Auto Socorro
               </Text>
             </Box>
           </Link>
         </Flex>
-        
-        <Link href="/" mt="8" style={{ textDecoration:"none" }} >
-          <Text _hover={{ color: "primary", fontWeight:"600" }} fontSize=".875rem" >
-            IR PARA O SITE 
+
+        <Link href="/" mt="8" style={{ textDecoration: "none" }}>
+          <Text
+            _hover={{ color: "primary", fontWeight: "600" }}
+            fontSize=".875rem"
+          >
+            IR PARA O SITE
           </Text>
         </Link>
-
       </Flex>
     </Flex>
-  )
+  );
 }
